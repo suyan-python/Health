c0030de2acd0f3aeb2260b274a387ab44ea5f50d
Suyan Man Amatya <srkamatya007@gmail.com>
2024-08-31 21:53:09 +0545

    About section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Doc from "../assets/doctors2.png";
+import Logo from "../assets/web-logo2.png";
+import Button from "../layouts/Button";
 
 function About() {
-  return <div>About Section</div>;
+  return (
+    <>
+      <div className="flex flex-col justify-center h-2/4 rounded-3xl shadow-sm bg-gradient-to-t from-green-100 to-blue-200 items-center text-center">
+        <div className="text-area flex flex-col w-3/4 justify-center items-center pl-20 ">
+          <div className="slog rounded-full flex w-1/5 justify-center px-3 py-2 ">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="title text-5xl py-3 w-2/4">
+            <h1 className="font-semibold">We're changing the whole game.</h1>
+          </div>
+          <div className="description text-slate-400"></div>
+          <div className="book py-2">
+            <Button title="Connect with our Professionals ↗" />
+          </div>
+        </div>
+        <div className="flex justify-center image-area  w-3/4 ">
+          <img style={{ height: "50vh" }} src={Doc} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
--- client/src/layouts/Button.jsx
+++ client/src/layouts/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <div>
-      <button className="bg-green-500 px-5 py-1 rounded-full text-white hover:text-blue-200">
+      <button className="bg-green-500 px-5 py-1 rounded-full text-white hover:text-blue-200 font-medium">
         {props.title}
       </button>
     </div>

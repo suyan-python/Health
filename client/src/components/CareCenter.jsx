55b9aaf8d25274784d40d1abbb0840cdaf6a8136
Rii911 <ritu.maharjan@acme.edu.np>
2024-08-31 20:25:33 +0545

    1st change

--- client/src/components/CareCenter.jsx
+++ client/src/components/CareCenter.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function CareCenter() {
-  return <div>Care Center</div>;
+  return <div>
+    Care Center
+    <div className="nums">
+      police..... 
+      <a href="tel:+977 9841188132">+977 9841188132</a>
+    </div>
+  </div>;
 }
 
 export default CareCenter;

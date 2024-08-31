fe0fcd5cdb8a34e6dcab8c86757a639eb0ad7df9
Suyan Man Amatya <srkamatya007@gmail.com>
2024-08-31 15:27:59 +0545

    My branch push

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Landing() {
-  return <div className="bg-red-300 text-red-500">Landing</div>;
+  return (
+    <>
+      <div className="bg-red-300 text-red-500">This is Suyan Branch</div>
+      <p>where you can find wounderful landing page</p>
+    </>
+  );
 }
 
 export default Landing;

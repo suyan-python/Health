bcee79e6907cccb7559e915270209e92739eb39b
Rii911 <ritu.maharjan@acme.edu.np>
2024-09-02 21:50:57 +0545

    login-pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import CareCenter from "./components/CareCenter";
 import Landing from "./components/landing";
 // import SignIp from "./components/log/SignIn";
 import Navbar from "./components/Navbar";
+import Doclogin from "./components/log/Doc-login";
+import Adminlogin from "./components/log/Admin-login";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -16,6 +19,8 @@ function App() {
           <Route path="/dashboard" element={<Admindashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/carecenter" element={<CareCenter />} />
+          <Route path="/Doc-login" element={<Doclogin/>}/>
+          <Route path="/Admin-login" element={<Adminlogin/>}/>
         </Routes>
       </Router>
     </>
